87b21a8c6f7edfb05f79603557bf6bb0394fffa5
ryuki88 <y21513@iwate-it.ac.jp>
2022-01-17 12:47:05 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,17 +2,17 @@ function stop1(){
     //画像ファイル名を配列で保持
     const image1 = [
         "images/hasuto_1.PNG",
-        "images/koya_1.PNG",
-        "images/komatu_1.PNG",
-        "images/komatu2_1.PNG",
-        "images/rin_1.PNG",
+        "images/koya_1.png",
+        "images/komatu_1.png",
+        "images/komatu2_1.png",
+        "images/rin_1.png",
         "images/takuma_1.PNG",
-        "images/takumi_1.PNG",
+        "images/takumi_1.png",
         "images/ube_1.PNG", 
-        "images/yoshiki_1.PNG",  
+        "images/yoshiki_1.png",  
         "images/yusaku_1.PNG", 
         "images/yusaku2_1.PNG",
-        "images/yuya_1.PNG"
+        "images/yuya_1.png"
     ];
     
     //ランダムで画像ファイルを取得して表示する
@@ -25,17 +25,17 @@ function stop2(){
     //画像ファイル名を配列で保持
     const image2 = [
         "images/hasuto_2.PNG",
-        "images/koya_2.PNG",
-        "images/komatu_2.PNG",
-        "images/komatu2_2.PNG",
-        "images/rin_2.PNG",
+        "images/koya_2.png",
+        "images/komatu_2.png",
+        "images/komatu2_2.png",
+        "images/rin_2.png",
         "images/takuma_2.PNG",
-        "images/takumi_2.PNG",
+        "images/takumi_2.png",
         "images/ube_2.PNG", 
-        "images/yoshiki_2.PNG",  
+        "images/yoshiki_2.png",  
         "images/yusaku_2.PNG", 
         "images/yusaku2_2.PNG",
-        "images/yuya_2.PNG"
+        "images/yuya_2.png"
     ];
     
     //ランダムで画像ファイルを取得して表示する
@@ -48,17 +48,17 @@ function stop3(){
     //画像ファイル名を配列で保持
     const image3 = [
         "images/hasuto_3.PNG",
-        "images/koya_3.PNG",
-        "images/komatu_3.PNG",
-        "images/komatu2_3.PNG",
-        "images/rin_3.PNG",
+        "images/koya_3.png",
+        "images/komatu_3.png",
+        "images/komatu2_3.png",
+        "images/rin_3.png",
         "images/takuma_3.PNG",
-        "images/takumi_3.PNG",
+        "images/takumi_3.png",
         "images/ube_3.PNG", 
-        "images/yoshiki_3.PNG",  
+        "images/yoshiki_3.png",  
         "images/yusaku_3.PNG", 
         "images/yusaku2_3.PNG",
-        "images/yuya_3.PNG"
+        "images/yuya_3.png"
     ];
     
     //ランダムで画像ファイルを取得して表示する
